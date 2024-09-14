006bcc4f078ac1c6daa7f5e2e11fe4447d134606
Mnangat N <mnangatnathan01@gmail.com>
2024-09-14 12:46:19 +0300

    changes

--- src/app/components/clientpathname.tsx
+++ src/app/components/clientpathname.tsx
@@ -1,10 +1,9 @@
 "use client"; // This directive makes this file a client component
 
-import { usePathname } from "next/navigation";
 import RootLayout from "../layout";
 
 export default function ClientPathname({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
   
-  return <RootLayout pathname={pathname}>{children}</RootLayout>;
+  
+  return <RootLayout>{children}</RootLayout>;
 }
